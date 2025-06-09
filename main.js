a3d7761c6de133f293255284bc223cadd280ea77
first-thirteenth <tpatata13@gmail.com>
2025-06-09 11:39:00 +0200

    Age

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 console.log("Hi!")
-alert.
+alert("Я купил шишку:)")
 document.writeln(`<h1>Hello, World! ${12 + 5}</h1>`)
 
 /*-----------------Создавать переменные let---------*/
 let age
 age = 13
 console.log(age)
+
+/*---------------------ПРИМЕР---------------------*/
+let currentYear = 2025
+let age1 = currentYear - 1985
+let age2 = currentYear - 1988
+let age3 = currentYear - 2020
+document.writeln(`<p>Возраст пользователя: ${age1}</p>`)
+document.writeln(`<p>Возраст пользователя: ${age2}</p>`)
+document.writeln(`<p>Возраст пользователя: ${age3}</p>`)
