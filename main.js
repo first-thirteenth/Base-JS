31e7a6bc69073b8e5eb420d44839efaa9deda27a
first-thirteenth <tpatata13@gmail.com>
2025-06-09 11:16:51 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.writeln("<h1>Hello, World!</h1>")
+console.log("Hi!")
+alert.
+document.writeln(`<h1>Hello, World! ${12 + 5}</h1>`)
+
+/*-----------------Создавать переменные let---------*/
+let age
+age = 13
+console.log(age)
