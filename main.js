d26606d9b63cb1dcb8a112ea5a6f286b13c8f7a9
first-thirteenth <tpatata13@gmail.com>
2025-06-09 14:39:18 +0200

    new file

---
+++ main.js
@@ -0,0 +1,16 @@
+let index = 1
+document.writeln("<h1>Список пользователей:</h1>")
+
+let userName1 = "Андрей"
+let userAge1 = 2025-1985
+document.writeln(`<p>${index}) <strong>${userName1}</strong>, возраст: <strong>${userAge1}</strong></p>`)
+
+let userName2 = "Настя"
+let userAge2 = 2025-1988
+index = index + 1
+document.writeln(`<p>${index}) <strong>${userName2}</strong>, возраст: <strong>${userAge2}</strong></p>`)
+
+let userName3 = "Ян"
+let userAge3 = 2025-2020
+index = index + 1
+document.writeln(`<p>${index}) <strong>${userName3}</strong>, возраст: <strong>${userAge3}</strong></p>`)
---
+++ main2.js
@@ -0,0 +1,25 @@
+document.writeln(`<h1>Задание 2</h1>
+<p>Параграф с произвольным текстом</p>
+<ol>
+    <li>Элемент списка 1 и <a href = "#">ссылка</a></li>
+    <li>Элемент списка 2</li>
+    <li>Элемент списка 3</li>
+</ol>`)
+
+
+let userName = "Оля"
+let age = 19
+let height = 157
+document.writeln(`<h1>Анкета</h1>
+<ul>
+    <li>Имя: <strong>${userName}</strong></li>
+    <li>Возраст: <strong>${age}</strong></li>
+    <li>Год рождения: <strong>${2025 - age}</strong></li>
+    <li>Рост: <strong>${height}</strong></li>
+</ul>`)
+ 
+
+
+
+
+
